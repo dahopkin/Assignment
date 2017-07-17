fd2cf7e898ada04d10c2b680eea9722663b34d11
dahopkin <dahopkin2@gmail.com>
2017-07-17 10:46:07 -0400

    Added Home Controller and necessary views
    Added a controller and all the files that will be needed for the pages.
    This includes a landing page, a sign up form, and a "Thanks" page.

---
+++ Assignment.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment.Domain.Entities;
+
+namespace Assignment.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult SignUp(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to the party organizer.
+                return View("Thanks", customer);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+    }
+}

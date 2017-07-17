9566fc0fdc0a3fcfbb6925259193fe61dd033c83
dahopkin <dahopkin2@gmail.com>
2017-07-17 09:06:23 -0400

    Created Customer class.
    Created a class to represent the customers that will be saved with this
    application.

---
+++ Assignment.Domain/Entities/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Domain.Entities
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "The First Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The Last Name is required")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "The Birth Date is required")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "The Email Address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "The Number of Dependents is required")]
+        [Range(0, 50, ErrorMessage = "The Number of Dependents must be between 0 and 50")]
+        public int NumberOfDependents { get; set; }
+    }
+}
